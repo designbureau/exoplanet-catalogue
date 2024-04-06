1046b352fb6fba17a2c1a6fae703db57be4feaa0
ianjamieson <ian.jamieson@swankyagency.com>
2024-04-06 19:45:23 +0100

    refactor camera

--- app/components/Binary.jsx
+++ app/components/Binary.jsx
@@ -2,8 +2,9 @@ import { useRef, useContext, useEffect } from "react";
 import { RefContext } from "./RefContext";
 import Star from "./Star";
 import Planet from "./Planet";
+import { getPosition } from "../utils/helperFunctions";
 
-const Binary = ({ data }) => {
+const Binary = ({ data, parentPosition = { x: 0, y: 0, z: 0 } }) => {
   if (!data) return;
 
   const ref = useRef();
@@ -20,16 +21,54 @@ const Binary = ({ data }) => {
     setActive(ref);
   };
 
+  // Calculate the binary's own position based on separation and position angle from its parent
+  const separation = parseFloat(data.separation?.[0] ?? 0);
+  const positionAngleDegrees = parseFloat(data.positionangle?.[0] ?? 0);
+  const binaryPosition = getPosition({ separation, positionAngleDegrees });
+
+  // Adjust the binary's position based on the parentPosition
+  const adjustedBinaryPosition = {
+    x: parentPosition.x + binaryPosition.x,
+    y: parentPosition.y + binaryPosition.y,
+    z: parentPosition.z + binaryPosition.z,
+  };
+
+  const firstStarPosition = { ...adjustedBinaryPosition };
+  const secondStarPosition = getPosition({ separation, positionAngleDegrees });
+  secondStarPosition.x += adjustedBinaryPosition.x;
+  secondStarPosition.y += adjustedBinaryPosition.y;
+  secondStarPosition.z += adjustedBinaryPosition.z;
+
   const isActive = activeRef === ref;
 
   return (
-    <group ref={ref} name={name} active={isActive} onClick={handleClick}>
+    <group
+      ref={ref}
+      name={name}
+      active={isActive}
+      // onClick={handleClick}
+      position={[
+        adjustedBinaryPosition.x,
+        adjustedBinaryPosition.y,
+        adjustedBinaryPosition.z,
+      ]}
+    >
       {data.binary &&
         data.binary.map((binary, index) => (
-          <Binary key={index} data={binary} />
+          <Binary
+            key={index}
+            data={binary}
+            parentPosition={adjustedBinaryPosition}
+          />
         ))}
       {data.star &&
-        data.star.map((star, index) => <Star key={index} data={star} />)}
+        data.star.map((star, index) => (
+          <Star
+            key={index}
+            data={star}
+            position={index === 0 ? firstStarPosition : secondStarPosition}
+          />
+        ))}
       {data.planet &&
         data.planet.map((planet, index) => (
           <Planet key={index} data={planet} />
